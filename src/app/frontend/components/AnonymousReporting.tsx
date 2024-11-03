7f9cd25ec2ddf94f2834ebb44b631fb28271c059
Lestlin Robins <lestlinr@gmail.com>
2024-11-03 15:16:52 +0530

    Added the anonymous reporting section

--- src/app/frontend/components/AnonymousReporting.tsx
+++ src/app/frontend/components/AnonymousReporting.tsx
@@ -1,7 +1,14 @@
 import React, { useState, useRef, useEffect } from "react";
-import supabase from "../supabase"; // Adjust path as needed
+import { createBrowserClient } from "@supabase/ssr";
 import "./AnonymousReporting.css"; // For styling
 import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
+export const createClient = () => {
+  return createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
+};
+const supabase = createClient();
 
 const AnonymousReporting = () => {
   const [description, setDescription] = useState("");
